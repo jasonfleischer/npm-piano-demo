6b60e22ad452d65d9b3ea833557d304d7838fa54
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-09-10 18:36:09 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,18 +6,17 @@
 # $ npm install -g uglify-js
 # $ npm install -g browserify
 
-CWD=`pwd`
-
 
 #npm init -y
 #npm i simple-piano --save
 #npm update
 
-uglifyjs js/module.js js/main.js -o js/bundle.js
+CWD=`pwd`
+
+uglifyjs js/main.js -o js/bundle.js
 browserify js/bundle.js -o js/bundle.js
 
 git add *; git commit -m 'update'; git push;
-
 
 cd $CWD
 
