2185bd980baeb9c08b5f0e09b577de70081a2c0d
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-09-10 21:39:30 -0400

    up

--- js/module.js
+++ js/module.js
@@ -1,7 +1,12 @@
-import { simplePiano } from './simple-piano.js';
+/*import { simplePiano } from './simple-piano.js';
 
 simplePiano({
 	range: '10',
 	interactive: false,
 	width: 700
-})
+})*/
+
+import {log, logE} from "./module.js"
+
+log("test")
+logE("tests")
