2f7693174adc13e1fd1c26b239be350517d0d9cc
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-09-10 21:58:27 -0400

    up

--- js/main.js
+++ js/main.js
@@ -1,16 +1,22 @@
-//const = require("logKit")
+import {log, logE} from "../node_module/simple-piano/index.js"
 
-import {log, logE} from "./module.js"
+/*import { simplePiano } from './simple-piano.js';
+
+simplePiano({
+	range: '10',
+	interactive: false,
+	width: 700
+})*/
 
 log("test")
 logE("tests")
 
 function drawNote() {
-	//logE('test')
+	logE('test')
 }
 
 function drawChord() {
-	//log('test 2')
+	log('test 2')
 }
 
 function drawScale() {
