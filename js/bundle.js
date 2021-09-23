015bf7fdd892c8f365722d915b34877a221230d9
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-09-22 20:18:50 -0400

    update

--- js/bundle.js
+++ js/bundle.js
@@ -1,6 +1,6 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
 const piano=require("@jasonfleischer/piano");const log=require("@jasonfleischer/log");piano.init({range:"10",interactive:false,width:700});document.getElementById("note_button").onclick=function(){midiValue=75;piano.drawNote(midiValue)};document.getElementById("chord_button").onclick=function(){log.e("testhvhgvhg")};document.getElementById("scale_button").onclick=function(){log.i("testhgbhgv 2")};document.getElementById("clear_button").onclick=function(){piano.clear()};
-},{"@jasonfleischer/log":2,"@jasonfleischer/piano":4}],2:[function(require,module,exports){
+},{"@jasonfleischer/log":2,"@jasonfleischer/piano":5}],2:[function(require,module,exports){
 var LOG_NON_ERROR_MESSAGES = true;
 const log = {};
 
@@ -20,6 +20,220 @@ log.turnOffNonErrorLogs = function() {
 module.exports = log;
 
 },{}],3:[function(require,module,exports){
+
+const CHORD_TYPE = Object.freeze({
+	Major: "Major",
+	minor: "minor",
+  Aug: "augmented",
+  Dim: "diminished",
+
+	Major7: "Major 7",
+  minor7: "minor 7",
+  Dom7: "Dominant 7"
+});
+
+const CHORD_INVERSION_TYPE = Object.freeze({
+  Root: "Root",
+  First: "first inversion",
+  Second: "second inversion",
+  Third: "third inversion"
+});
+
+const CHORD_PLAY_TYPE = Object.freeze({
+    HARMONIC: "Harmonic",
+    ARPEGGIATE: "Arpeggiate"
+});
+const ALL_CHORD_TYPES = [CHORD_TYPE.Major, CHORD_TYPE.minor, CHORD_TYPE.Major7, CHORD_TYPE.minor7];
+
+
+class Chord {
+
+
+  constructor(root_note, chord_type, play_type, inversion) {
+    this.delay_in_ms = 500;
+    this.name = root_note.note_name.name + " " + chord_type;
+    this.inversion = inversion
+    this.type = chord_type
+    this.play_type = play_type
+
+    switch(chord_type){
+    	case CHORD_TYPE.Major:
+        if(this.inversion == CHORD_INVERSION_TYPE.Root) {
+    		  this.note_array = [root_note, all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7]];
+          this.note_labels = ["R", "M3", "5"];
+          this.structure = "Root, Major 3rd, Fifth";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.First){
+          this.note_array = [all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 12]];
+          this.note_labels = ["M3", "5", "R"];
+          this.structure = "Major 3rd, Fifth, Root";
+        } else {
+          this.note_array = [all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 16]];
+          this.note_labels = ["5", "R", "M3"];
+          this.structure = "Fifth, Root, Major 3rd"; 
+        }
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/major.mp3"]);
+    		break;
+    	case CHORD_TYPE.minor:
+        if(this.inversion == CHORD_INVERSION_TYPE.Root) {
+          this.note_array = [root_note, all_notes[root_note.note_value + 3], all_notes[root_note.note_value + 7]];
+          this.note_labels = ["R", "m3", "5"];
+          this.structure = "Root, minor 3rd, Fifth";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.First){
+          this.note_array = [all_notes[root_note.note_value + 3], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 12]];
+          this.note_labels = ["m3", "5", "R"];
+          this.structure = "minor 3rd, Fifth, Root";
+        } else {
+          this.note_array = [all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 15]];
+          this.note_labels = ["5", "R", "m3"];
+          this.structure = "Fifth, Root, minor 3rd"; 
+        }
+    		
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/minor.mp3"]);
+    		break;
+
+      case CHORD_TYPE.Aug:
+        this.inversion = CHORD_INVERSION_TYPE.Root;
+        this.note_array = [root_note, all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 8]];
+        this.note_labels = ["R", "M3", "#5"];
+        this.structure = "Root, Major 3rd, Sharp Fifth";
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/augmented.mp3"]);
+        break;
+      case CHORD_TYPE.Dim:
+        this.inversion = CHORD_INVERSION_TYPE.Root;
+        this.note_array = [root_note, all_notes[root_note.note_value + 3], all_notes[root_note.note_value + 6]];
+        this.note_labels = ["R", "m3", "b5"];
+        this.structure = "Root, minor 3rd, Flat Fifth";
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/diminished.mp3"]);
+        break;
+
+    	case CHORD_TYPE.Major7:
+
+        if(this.inversion == CHORD_INVERSION_TYPE.Root) {
+          this.note_array = [root_note, all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 11]];
+          this.note_labels = ["R", "M3", "5", "M7"];
+          this.structure = "Root, Major 3rd, Fifth, Major 7th";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.First){
+          this.note_array = [all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 11], all_notes[root_note.note_value + 12]];
+          this.note_labels = ["M3", "5", "M7", "R"];
+          this.structure = "Major 3rd, Fifth, Major 7th, Root";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.Second){
+          this.note_array = [all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 11], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 16]];
+          this.note_labels = ["5", "M7", "R", "M3"];
+          this.structure = "Fifth, Major 7th, Root, Major 3rd";
+        } else {
+          this.note_array = [all_notes[root_note.note_value + 11], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 16], all_notes[root_note.note_value + 19]];
+          this.note_labels = ["M7", "R", "M3", "5"];
+          this.structure = "Major 7th, Root, Major 3rd, Fifth";
+        }
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/major_seventh.mp3"]);
+        break;
+      case CHORD_TYPE.minor7:
+
+
+        if(this.inversion == CHORD_INVERSION_TYPE.Root) {
+          this.note_array = [root_note, all_notes[root_note.note_value + 3], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10]];
+          this.note_labels = ["R", "m3", "5", "m7"];
+          this.structure = "Root, minor 3rd, Fifth, minor 7th";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.First){
+          this.note_array = [all_notes[root_note.note_value + 3], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12]];
+          this.note_labels = ["m3", "5", "m7", "R"];
+          this.structure = "minor 3rd, Fifth, minor 7th, Root";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.Second){
+          this.note_array = [all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 15]];
+          this.note_labels = ["5", "m7", "R", "m3"];
+          this.structure = "Fifth, minor 7th, Root, minor 3rd";
+        } else {
+          this.note_array = [all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 15], all_notes[root_note.note_value + 19]];
+          this.note_labels = ["m7", "R", "m3", "5"];
+          this.structure = "minor 7th, Root, minor 3rd, Fifth";
+        }
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/minor_seventh.mp3"]);
+        break;
+
+      case CHORD_TYPE.Dom7:
+        
+        if(this.inversion == CHORD_INVERSION_TYPE.Root) {
+          this.note_array = [root_note, all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10]];
+          this.note_labels = ["R", "M3", "5", "7"];
+          this.structure = "Root, Major 3rd, Fifth, 7th";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.First){
+          this.note_array = [all_notes[root_note.note_value + 4], all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12]];
+          this.note_labels = ["M3", "5", "7", "R"];
+          this.structure = "Major 3rd, Fifth, 7th, Root";
+        } else if (this.inversion == CHORD_INVERSION_TYPE.Second){
+          this.note_array = [all_notes[root_note.note_value + 7], all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 16]];
+          this.note_labels = ["5", "7", "R", "M3"];
+          this.structure = "Fifth, 7th, Root, Major 3rd";
+        } else {
+          this.note_array = [all_notes[root_note.note_value + 10], all_notes[root_note.note_value + 12], all_notes[root_note.note_value + 16], all_notes[root_note.note_value + 19]];
+          this.note_labels = ["7", "R", "M3", "5"];
+          this.structure = "7th, Root, Major 3rd, Fifth";
+        }
+        this.file_name = root_note.note_name.file_name.concat(["audio/chords/dominant_seventh.mp3"]);
+        break;
+
+    }
+  }
+
+  to_string() {
+    return  "CHORD: " + this.name +", "+ this.structure + ", ", this.note_array;
+  }
+}
+
+function type_is_three_notes(chord_type) {
+  return chord_type == CHORD_TYPE.Major || chord_type == CHORD_TYPE.minor || chord_type == CHORD_TYPE.Aug || chord_type == CHORD_TYPE.Dim 
+}
+
+function generate_random_chord(min, max){
+
+  var chord_array = []
+  if(model.chords.three_note_types.length == 0 && model.chords.four_note_types.length == 0){
+    logE("fatal error: generate_random_chord")
+  } else if (model.chords.three_note_types.length > 0 && model.chords.four_note_types.length == 0){
+    chord_array = model.chords.three_note_types
+  }else if (model.chords.three_note_types.length == 0 && model.chords.four_note_types.length > 0){
+    chord_array = model.chords.four_note_types
+  }else {
+    chord_array = model.chords.three_note_types.concat(model.chords.four_note_types);
+  }
+
+  var random_note = generate_random_note(min, max-18);// hack so it doesn't go out of bounds
+  var random_chord_type = chord_array[ randomIntFromInterval(0, chord_array.length-1) ];
+
+  var play_type = model.chords.play_types[ randomIntFromInterval(0, model.chords.play_types.length-1) ];
+  var inversion = CHORD_INVERSION_TYPE.Root;
+  if (type_is_three_notes(random_chord_type)){
+    inversion = model.chords.three_note_inversion_types[ randomIntFromInterval(0, model.chords.three_note_inversion_types.length-1) ];
+  } else{
+    inversion = model.chords.four_note_inversion_types[ randomIntFromInterval(0, model.chords.four_note_inversion_types.length-1) ];
+  }
+  var random_chord = new Chord(random_note, random_chord_type, play_type, inversion);
+
+  return random_chord;
+}
+
+
+function generate_chord_with_note(note_name){
+
+  var random_chord_type = ALL_CHORD_TYPES[ randomIntFromInterval(0, ALL_CHORD_TYPES.length-1) ];
+  var octave = 1 + randomIntFromInterval(2, 4);// limit to lower register
+  var note = all_notes[note_name.associated_note_values[octave]];
+
+  var play_type = CHORD_PLAY_TYPE.HARMONIC;
+  var inversion = CHORD_INVERSION_TYPE.Root;
+  var random_chord = new Chord(note, random_chord_type, play_type, inversion);
+
+  return random_chord;
+
+}
+
+
+module.exports = {Chord, CHORD_TYPE, CHORD_INVERSION_TYPE} 
+
+
+},{}],4:[function(require,module,exports){
+const chord = require("./chord.js");
+
 class Note {
   constructor(note_name, note_value, octave) {
     this.note_name = note_name;
@@ -213,10 +427,10 @@ function init(){
 }
 
 
-module.exports = {Note, NoteName, init, all_notes};
+module.exports = {Note, NoteName, init, all_notes, chord};
 
 
-},{}],4:[function(require,module,exports){
+},{"./chord.js":3}],5:[function(require,module,exports){
 const musicKit = require("@jasonfleischer/music-model-kit");
 
 
@@ -374,7 +588,7 @@ piano_view.clearCanvases = function(){
 
 piano_view.drawNote = function(note){
 
-	piano_view.clearCanvases();
+	piano_view.clearCanvases(); // todo rm?
 	piano_view.drawNoteWithColor(note);
 }
 
@@ -500,6 +714,10 @@ piano.drawNote = function(midiValue) {
 	piano_view.drawNote(musicKit.all_notes[midiValue]);
 }
 
+piano.drawChord = function(chord) {
+	piano_view.drawNote(chord);
+}
+
 piano.clear = function(midiValue) {
 	piano_view.clearCanvases();
 }
@@ -507,4 +725,4 @@ piano.clear = function(midiValue) {
 module.exports = piano;
 
 
-},{"@jasonfleischer/music-model-kit":3}]},{},[1]);
+},{"@jasonfleischer/music-model-kit":4}]},{},[1]);
