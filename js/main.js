ee790f51e4bceacfdaa82b1694da4073ba714417
Jason Fleischer <jason_fleischer@hotmail.ca>
2021-09-27 19:58:42 -0400

    update

--- js/main.js
+++ js/main.js
@@ -4,31 +4,33 @@ const pianoKit = require("@jasonfleischer/piano")
 const musicKit = require("@jasonfleischer/music-model-kit");
 musicKit.init();
 
-let pianoView = pianoKit.buildPianoView({
-	id: 'display_piano'
-	,width: 700
-});
+{
+	let pianoView = pianoKit.buildPianoView({
+		id: 'display_piano'
+		,width: 700
+	});
 
-document.getElementById("note_button").onclick = function() { 
-	let midiValue = 45; // A2
-	let note = musicKit.all_notes[midiValue];
-	pianoView.clear();
-	pianoView.drawNote(note);
-};
-document.getElementById("chord_button").onclick = function() {
-	let midiValue = 60 // C4
-	let note = musicKit.all_notes[midiValue];
-	let chord = new musicKit.Chord(note, musicKit.Chord.TYPE.minor);
-	pianoView.drawChord(chord);
-}
-document.getElementById("scale_button").onclick = function() {
-	let midiValue = 60 // C
-	let note = musicKit.all_notes[midiValue];
-	let scale = new musicKit.Scale(note, musicKit.Scale.TYPE.Aeolian);
-	pianoView.drawScale(scale);
-}
-document.getElementById("clear_button").onclick = function() {
-	pianoView.clear();
+	document.getElementById("note_button").onclick = function() { 
+		let midiValue = 45; // A2
+		let note = musicKit.all_notes[midiValue];
+		pianoView.clear();
+		pianoView.drawNote(note);
+	};
+	document.getElementById("chord_button").onclick = function() {
+		let midiValue = 60 // C4
+		let note = musicKit.all_notes[midiValue];
+		let chord = new musicKit.Chord(note, musicKit.Chord.TYPE.minor);
+		pianoView.drawChord(chord);
+	}
+	document.getElementById("scale_button").onclick = function() {
+		let midiValue = 60 // C
+		let note = musicKit.all_notes[midiValue];
+		let scale = new musicKit.Scale(note, musicKit.Scale.TYPE.Aeolian);
+		pianoView.drawScale(scale);
+	}
+	document.getElementById("clear_button").onclick = function() {
+		pianoView.clear();
+	}
 }
 
 {
