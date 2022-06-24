c4fea6aa492e36d804b1dd3892827f014f31b900
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-06-23 20:02:25 -0400

    update

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,8 +6,6 @@
 # $ npm install -g uglify-js
 # $ npm install -g browserify
 
-CWD=`pwd`
-
 npm update;
 
 uglifyjs js/main.js -o js/bundle.js
@@ -15,7 +13,5 @@ browserify js/bundle.js -o js/bundle.js
 uglifyjs js/bundle.js -o js/bundle.js
 
 git add *; git commit -m 'update'; git push;
-
-cd $CWD
 
 
